c84e57abc14c5004c871116e2979fa70784e480e
Ray <117043408+Snowlydial@users.noreply.github.com>
2026-03-29 20:55:40 +0300

    Create ArticleRepository.java

---
+++ src/main/java/com/itu/miniprojet/repository/ArticleRepository.java
@@ -0,0 +1,11 @@
+package com.itu.miniprojet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.itu.miniprojet.model.Article;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+    
+}
