b90ca9d54e31953802c6c47fe06ba2d848600207
Ray <117043408+Snowlydial@users.noreply.github.com>
2026-03-29 12:52:43 +0300

    Initial commit

---
+++ src/main/java/com/itu/miniprojet/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.itu.miniprojet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MiniprojetApplication.class);
+	}
+
+}
